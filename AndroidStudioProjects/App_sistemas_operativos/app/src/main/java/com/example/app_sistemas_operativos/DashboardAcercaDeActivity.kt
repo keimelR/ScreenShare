e7777922d61dde238826d7a90f5da697e4139f3e
keimelR <keimelrodriguez12@gmail.com>
2024-07-15 05:39:20 +0200

    1- Se redimensiono los Botones de dashboard_client.xml 2- Se añadio una descripcion a dashboard_acerca_de.xml 3- Se linkeo al DashboardAcercaDeActivity.kt el layout dashboard_acerca_de.xml

--- AndroidStudioProjects/App_sistemas_operativos/app/src/main/java/com/example/app_sistemas_operativos/DashboardAcercaDeActivity.kt
+++ AndroidStudioProjects/App_sistemas_operativos/app/src/main/java/com/example/app_sistemas_operativos/DashboardAcercaDeActivity.kt
@@ -1,6 +1,12 @@
 package com.example.app_sistemas_operativos
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class DashboardAcercaDeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dashboard_acerca_de)
+
+    }
 }
